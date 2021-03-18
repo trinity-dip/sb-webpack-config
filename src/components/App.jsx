70fc787d5eb48b79ffbf2757aac72dc7198c7404
VYARA <vyara.angelova@gmx.net>
2021-03-18 02:50:36 +0100

    Image support zum Projekt hinzugefügt.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import anton from "../images/anton_2.png";
+import haus from "../images/Icon_Haus.svg";
 
 //In diesem Fall nicht nötig, denn es wird nur JSX in dieser Datei benutzt...(ohne React)
 //import React from "react";
@@ -12,11 +14,12 @@ const App = () => {
         <section>
           <h1>Oh Hai, React!</h1>
         </section>
+        <img src={anton} alt="Anton in love" width="250" />
+        <img src={haus} alt="Anton in love" width="250" />
+        <Recipes />
       </main>
-
-      <Recipes /> 
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,29 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules:[
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+        //parser: {
+          //dataUrlCondition: {
+            //maxSize: 30 * 1024,
+          //}
+        //}
+      },
+      {
         //test: /\.s?css$/i,
         test: /\.(s[ac]|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader"

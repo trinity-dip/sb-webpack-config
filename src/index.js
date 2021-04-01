606c5b476dc6768ad7af22da0dbd49c18ec67840
VYARA <vyara.angelova@gmx.net>
2021-04-01 14:25:31 +0200

    Bootstrap zum Projekt hinzugefüg + @popperja/core (wird von Bootstrap erfordert)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { render } from "react-dom";
 import App from "./components/App";
+import "../node_modules/popper.js";
+import "../node_modules/bootstrap";
 
+
+require("bootstrap");
 render(<App />, document.getElementById("root"));
 
